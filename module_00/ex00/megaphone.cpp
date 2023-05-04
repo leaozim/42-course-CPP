09925c3045b6cc815143794ca2209a3a83f1dbe4
Larissa Silva <larissa_silva@outlook.com>
2023-05-04 17:05:35 -0300

    add corrections in space

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,14 +6,18 @@
 # define ERROR "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
 void	its_time_to_scream(char *the_scream) {
-	for (int i = 0; i < the_scream[i]; i++)
+	for (int i = 0; the_scream[i] != '\0'; i++)
 		the_scream[i] = toupper(the_scream[i]);
-	std::cout << the_scream << " ";
+	std::cout << the_scream;
 }
 
-void voice_amplifier(char **the_scream, int argc) {
-	for (int i = 1; i < argc; i++)
+void voice_amplifier(char **the_scream, int num_arg) {
+	for (int i = 1; i < num_arg; i++)
+	{
 		its_time_to_scream(the_scream[i]);
+		if (i != num_arg -1)
+			std::cout << " ";
+	}
 	std::cout << std::endl;
 }
 
