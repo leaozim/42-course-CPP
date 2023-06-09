1a78bbf93c4232e581aac0e321f1e0f137e7b5a5
Larissa Silva <larissa_silva@outlook.com>
2023-06-09 17:41:15 -0300

    add updates - module-05 (ex01)

--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -14,25 +14,26 @@ static void print_separator( void )
 	std::cout << CYAN << std::string(70, '-') << RES << std::endl;
 }
 
-int main()
+void	test_constructors( void )
 {
-	{
-		print_header("CONSTRUCTORS");
-		Form		f("Sign", 10, 15);
-		Bureaucrat	b("João", 5);
-		print_separator();
-		std::cout << f << std::endl;
-		print_separator();
-		std::cout << b << std::endl;
-		print_header("DESTRUCTORS");
-	}
-	{
+	print_header("CONSTRUCTORS");
+	Form		f("Sign", 10, 15);
+	Bureaucrat	b("João", 5);
+	print_separator();
+	std::cout << f << std::endl;
+	print_separator();
+	std::cout << b << std::endl;
+	print_header("DESTRUCTORS");
+}
+
+void	test_copy_constructor( void )
+{
 		print_header("CONSTRUCTORS");
 		Form       f1("Sign", 10, 20);
 		print_separator();
 		Form       f2(f1);
 		print_separator();
-		Form       f3(f1);
+		Form       f3 = f1;
 		print_separator();
 		Bureaucrat b1("Josefa", 5);
 		print_separator();
@@ -44,24 +45,30 @@ int main()
 		b2.signForm(f2);
 		b3.signForm(f3);
 		print_header("DESTRUCTORS");
+}
+
+void	test_constructors_with_error( void )
+{
+	print_header("CONSTRUCTORS - GradeTooHighException");
+	try {
+		Form f("Sign", MAX_GRADE - 1, 10);
 	}
-	{
-		print_header("CONSTRUCTORS - GradeTooHighException");
-		try {
-			Form f("Sign", MAX_GRADE - 1, 10);
-		}
-		catch (std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
 	}
-	{
-		print_header("CONSTRUCTORS - GradeTooLowException");
-		try {
-			Form f("Sign", 1, 150 + 1);
-		}
-		catch (std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
+	print_header("CONSTRUCTORS - GradeTooLowException");
+	try {
+		Form f("Sign", 1, 150 + 1);
 	}
-	return 0;
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+}
+
+int main()
+{
+	test_constructors();
+	test_copy_constructor();
+	test_constructors_with_error();
+	return (0);
 }
