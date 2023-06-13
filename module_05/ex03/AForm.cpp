db1099efad60cedec8d62d9fc3f8a417b3c60e68
Larissa Silva <larissa_silva@outlook.com>
2023-06-13 18:30:57 -0300

    add updates - module-05 (ex03)

---
+++ module_05/ex03/AForm.cpp
@@ -0,0 +1,113 @@
+#include "AForm.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+AForm::AForm() : _name("Chico"), _isSigned(false), _gradeSign(5), _gradeExecute(5) {
+	print_description("Default constructor called", ORANGE);
+}
+
+AForm::AForm( const AForm &src ) 
+	: _name(src.getName()), _isSigned(src.getGradeSign()), _gradeSign(src.getGradeSign()), _gradeExecute(src.getGradeExecute()) {
+	*this = src;
+	print_description("Copy constructor called", ORANGE);
+}
+
+AForm::AForm(std::string name, int gradeSign, int gradeExecute) 
+	: _name(name), _isSigned(false), _gradeSign(gradeSign), _gradeExecute(gradeExecute) {
+	if (gradeSign < MAX_GRADE || gradeExecute < MAX_GRADE) { 
+		throw GradeTooHighException(); }
+	else if (gradeSign > MIN_GRADE || gradeExecute > MIN_GRADE) { 
+		throw GradeTooLowException(); }
+	const_cast<std::string&>(this->_name) = name;
+	const_cast<int&>(this->_gradeSign) = gradeSign;
+	const_cast<int&>(this->_gradeExecute) = gradeExecute;
+	print_description("Name and Grades constructor called", ORANGE);
+}
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+AForm::~AForm() {
+	print_description("Destructor called", ORANGE);
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+AForm &				AForm::operator=( AForm const &rhs ) {
+	if ( this != &rhs )
+	{
+		const_cast<std::string&>(this->_name) = getName();
+		const_cast<int&>(this->_gradeSign) = getGradeSign();
+		const_cast<int&>(this->_gradeExecute) = getGradeExecute();
+		print_description("Copy assignment operator called", ORANGE);
+	}
+	return *this;
+}
+
+std::ostream		&operator<<( std::ostream &o, AForm const &i ) {
+	o	<< "Form name                 = " << i.getName() << std::endl
+		<< "Required Grade to Sign    = " << i.getGradeSign() << std::endl
+		<< "Required Grade to Execute = " << i.getGradeExecute() << std::endl
+		<< "Status                    = " << (i.getIsSigned() ? "Signed" : "Not Signed");
+	return (o);
+}
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+const char*				AForm::GradeTooHighException::what( void ) const throw() {
+	return ("Grade Too High Exception");
+}
+
+const char*				AForm::GradeTooLowException::what( void ) const throw() {
+	return ("Grade Too Low Exception");
+}
+
+const char*				AForm::NotSignedException::what( void ) const throw() {
+	return ("Not signed");
+}
+
+const char*				AForm::AlreadySignedException::what( void ) const throw() {
+	return ("Already signed");
+}
+
+void					AForm::beSigned( Bureaucrat &bureaucrat ) {
+	if (bureaucrat.getGrade() > this->_gradeSign) { throw GradeTooLowException(); }
+	if (this->_isSigned == true) { throw AlreadySignedException(); }
+	this->_isSigned = true;
+}
+
+void					AForm::checkExecute( Bureaucrat const &executor)  const {
+	if (executor.getGrade() > this->_gradeExecute) { throw GradeTooHighException(); }
+	if (!getIsSigned()) { throw NotSignedException(); }
+}
+
+void					AForm::print_description(std::string str, std::string color) {
+	std::cout << color << "AForm" << RES 
+			  << " = " 
+			  << str
+			  << std::endl;
+}	
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+std::string				AForm::getName( void ) const { return (this->_name); }
+
+int						AForm::getGradeSign( void ) const { return (this->_gradeSign); }
+
+int						AForm::getGradeExecute( void ) const { return (this->_gradeExecute); }
+
+bool					AForm::getIsSigned( void ) const { return (this->_isSigned) ;}
+
+
+/* ************************************************************************** */
---
+++ module_05/ex03/AForm.hpp
@@ -0,0 +1,64 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include "Bureaucrat.hpp"
+
+#define MAGENTA "\033[35m"
+#define RED "\x1b[38;5;88m"
+#define GREEN "\x1b[38;5;40m"
+#define BLUE "\x1b[38;5;25m"
+#define YELLOW "\x1b[38;5;220m"
+#define ORANGE	"\033[0;38;5;166m"
+#define LIGHT_PURPLE "\033[1;35m"
+#define CYAN "\001\e[0;36m\002"
+#define RES "\x1b[0m"
+
+class Bureaucrat;
+
+class AForm
+{
+
+	public:
+
+		AForm();
+		AForm( std::string name, int gradeSign, int gradeExecute );
+		AForm( AForm const &src );
+		~AForm();
+
+		AForm					&operator=( AForm const &rhs );
+
+		void					checkExecute( Bureaucrat const &executor ) const;
+		virtual void			execute( Bureaucrat const &executor ) const = 0;
+		void					beSigned( Bureaucrat &bureaucrat );
+		int						getGradeExecute (void ) const;
+		int						getGradeSign (void ) const;
+		bool					getIsSigned( void ) const;
+		std::string				getName( void ) const;
+		virtual void			print_description(std::string str, std::string color);
+
+		class GradeTooHighException: public std::exception {
+			virtual const char	*what( void ) const throw();
+		};
+		class GradeTooLowException: public std::exception {
+			virtual const char	*what( void ) const throw();
+		};
+		class NotSignedException: public std::exception {
+			virtual const char	*what( void ) const throw();
+		};
+		class AlreadySignedException: public std::exception {
+			virtual const char	*what( void ) const throw();
+		};
+
+	private:
+	
+		const std::string		_name;
+		bool					_isSigned;
+		const int				_gradeSign;
+		const int				_gradeExecute;
+
+
+};
+
+std::ostream			&operator<<( std::ostream &o, AForm const &i );
+
+#endif /* ************************************************************ FORM_H */
